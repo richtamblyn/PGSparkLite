5e7d26b5035483985733ff606771b381967a8b50
RichT <richard.tamblyn@gmail.com>
2021-01-17 00:39:30 +0000

    Added simple bash run script

---
+++ pgsparklite.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Really simple startup script to expose PGSparkLite to external users
+# Just make it executable and run it on startup
+# Whilst this will probably meet most users requirements, I recommend using Gunicorn for better resilience
+
+export FLASK_APP=app.py
+python3 -m flask run --host=0.0.0.0 &
