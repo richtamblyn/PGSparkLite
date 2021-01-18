6a419359a3bb963c0b92a8e905a5ce510a843bcb
RichT <richard.tamblyn@gmail.com>
2021-01-18 16:27:27 +0000

    Make script work with Supervisor

--- pgsparklite.sh
+++ pgsparklite.sh
@@ -2,7 +2,7 @@
 
 # Really simple startup script to expose PGSparkLite to external users
 # Just make it executable and run it on startup
-# Whilst this will probably meet most users requirements, I recommend using Gunicorn for better resilience
+# Whilst this will probably meet most users requirements, I recommend using Supervisor for better resilience
 
 export FLASK_APP=app.py
-python3 -m flask run --host=0.0.0.0 &
+python3 -m flask run --host=0.0.0.0
