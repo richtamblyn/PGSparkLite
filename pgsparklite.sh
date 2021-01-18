69aaf6a764095e32eaa83d9409920cf3ea52c43e
RichT <richard.tamblyn@gmail.com>
2021-01-18 18:59:54 +0000

    Add path change to bash script

--- pgsparklite.sh
+++ pgsparklite.sh
@@ -4,5 +4,8 @@
 # Just make it executable and run it on startup
 # Whilst this will probably meet most users requirements, I recommend using Supervisor for better resilience
 
+# Adjust this directory depending on your install directory
+cd /home/pi/PGSparkLite
+
 export FLASK_APP=app.py
 python3 -m flask run --host=0.0.0.0
